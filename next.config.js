a978bb50ca36f50900c8f6f16fad2c2ba9dadf13
BadCoder1337 <anton.tiz.musin@gmail.com>
2020-10-11 23:24:57 +0300

    Enable svg import

--- next.config.js
+++ next.config.js
@@ -7,14 +7,15 @@ process.env.SENTRY_DSN = SENTRY_DSN
 const config = {
   target: "server",
   webpack: (config, options) => {
+    // Replace sentry for browser
     if (!options.isServer) {
       config.resolve.alias["@sentry/node"] = "@sentry/browser"
     }
 
-    const pathToPolyfills = "./polyfills.js"
     // Fix [TypeError: Reflect.metadata is not a function] during production build
     // Thanks to https://leerob.io/blog/things-ive-learned-building-nextjs-apps#polyfills
     // There is an official example as well: https://git.io/JfqUF
+    const pathToPolyfills = "./polyfills.js"
     const originalEntry = config.entry
     config.entry = async () => {
       const entries = await originalEntry()
@@ -31,18 +32,19 @@ const config = {
       return entries
     }
 
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ["@svgr/webpack"],
+    })
+
     return config
   },
   poweredByHeader: false,
   distDir: "build",
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_SENTRY_DSN:
-      "https://4eaf09309fc3429d91e82b0c7117b9a9@sentry.rainbow6.ru/4",
-    NEXT_PUBLIC_GOOGLE_API_KEY: "AIzaSyDwEKMe2yFqkVn3BenJXVdzWO-LUOCdxvw",
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID:
-      "980295892804-5lc62u5ae72m7d3q7jsq90kt4dg7ddhp.apps.googleusercontent.com",
-  },
+  reactStrictMode: true
 }
 
 try {
