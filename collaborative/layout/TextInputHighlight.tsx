8fb5cdfbf69bcf05e450328c61907ff6941a23e1
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-01-13 02:38:59 +0300

    Selection design done

--- collaborative/layout/TextInputHighlight.tsx
+++ collaborative/layout/TextInputHighlight.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useEffect, useRef } from "react"
+import React, { forwardRef, useEffect, useRef, useState } from "react"
 import styled from "styled-components"
 import { Input } from "../../common/input/layout/Input"
 import { FlexContainer } from "../../common/layout/FlexContainer"
@@ -22,11 +22,10 @@ const SimpleTextInput = styled(Input)`
   }
 `
 
-// There's minor bugs in Firefox, i won't fix they
-
+// There's minor bugs in Firefox, I won't fix them
 const EchoInput = styled(Input)`
   position: absolute;
-  color: red;
+  color: transparent;
   /* Hide scrollbar*/
   ::-webkit-scrollbar-thumb, ::-webkit-scrollbar-track { background-color: transparent }
   scrollbar-color: transparent transparent;
@@ -51,16 +50,72 @@ const EchoInput = styled(Input)`
   }
 `
 
-type CommonInputElement = HTMLInputElement | HTMLTextAreaElement
+type CursorProps = {
+  color: string
+  // name: string
+}
+
+const Highlight = styled.span<CursorProps>`
+  position: relative;
+  background-color: ${props => `${props.color}7F`};
+  box-shadow: 0 0 0 1px ${props => props.color};
+  border-radius: 2px;
+`
+
+const Cursor = styled(Highlight)`
+  /* &:before { // this solution disappointed me because cursor name under the borders of input 
+    position: absolute;
+    font-size: 14px;
+    line-height: 14px;
+    top: -14px;
+    left: -2px;
+    content: "${props => props.color}";
+    background: ${props => props.color};
+    height: 14px;
+    z-index: 2;
+  } */
+  &:before {
+    position: absolute;
+    top: 0;
+    z-index: 2;
+    content: '';
+
+    width: 0; // CSS Triangle
+    height: 0;
+    border-style: solid;
+    border-width: 2px 0 2px 8px;
+    border-color: transparent transparent transparent ${props => props.color};
+  }
+`
+
+type CommonInputElement = HTMLInputElement | HTMLTextAreaElement | null
+
+type AnyProps = {
+  value: string;
+  id: string;
+  [s: string]: unknown;
+}
 
 const TextInputHighlight = (
+  props: AnyProps,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  props: any,
-  ref: React.ForwardedRef<CommonInputElement>
+  ref?: any
 ) => {
   const external = useRequiredContext(ExternalServiceManagerContext)
   const echoRef = useRef<HTMLDivElement>(null)
-  const inputRef = useRef(ref && "current" in ref ? ref.current : null) // use forwarded ref or create intermediate
+  const inputRef = useRef<CommonInputElement>(ref && "current" in ref ? ref.current : null) // use forwarded ref or create intermediate
+  const [pos, setPos] = useState([0, 0])
+
+  const { value } = props;
+  
+  const content = <>
+    {value.slice(0, pos[0])}
+    {pos[0] === pos[1]
+      ? <Cursor color="#FF0000" />
+      : <Highlight color="#FF0000">{value.slice(pos[0], pos[1])}</Highlight>
+    }
+    {value.slice(pos[1])}
+  </>
   
   useEffect(() => {
     const { current: echo } = echoRef
@@ -70,17 +125,19 @@ const TextInputHighlight = (
         echo.scrollTop  = input.scrollTop // echo.scrollHeight * (input.scrollTop  / input.scrollHeight)
         echo.scrollLeft = input.scrollLeft // echo.scrollWidth  * (input.scrollLeft / input.scrollWidth)
       }
+
+      setPos(new Array(2).fill(null).map(() => Math.round(Math.random() * value.length)).sort((a, b) => a - b))
   
       input.addEventListener("scroll", setScroll)
       return () => {
         input.removeEventListener("scroll", setScroll)
       }
     }
-  }, [inputRef])
+  }, [inputRef, value.length])
 
   return <HighlightContainer>
-    <SimpleTextInput ref={ref ?? inputRef} {...props} />
-    <EchoInput ref={echoRef} as="div">{props.id !== "webhook" && props.value}</EchoInput>
+    <SimpleTextInput style={{backgroundColor: "red"}} ref={ref ?? inputRef} {...props} />
+    <EchoInput ref={echoRef} as="div">{Boolean(value.length && props.id !== "webhook") && content}</EchoInput>
   </HighlightContainer>
 }
 
