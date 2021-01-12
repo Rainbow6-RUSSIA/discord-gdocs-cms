712c977416bb96b77414cb9cf9c3b051481d2589
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-01-12 18:33:21 +0300

    Perfect overlay done

--- collaborative/layout/TextInputHighlight.tsx
+++ collaborative/layout/TextInputHighlight.tsx
@@ -1,11 +1,16 @@
-import React, { forwardRef } from "react"
-import styled, { StyledComponentPropsWithRef, StyledProps  } from "styled-components"
+import React, { forwardRef, useEffect, useRef } from "react"
+import styled from "styled-components"
 import { Input } from "../../common/input/layout/Input"
-import type { InputFieldProps } from "../../common/input/text/InputField"
 import { FlexContainer } from "../../common/layout/FlexContainer"
-import type { ReactRef } from "../../common/state/ReactRef"
 import { useRequiredContext } from "../../common/state/useRequiredContext"
 import { ExternalServiceManagerContext } from "../header/ExternalServiceManagerContext"
+
+const HighlightContainer = styled(FlexContainer)`
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  background: ${({ theme }) => theme.background.secondaryAlt};
+`
 
 const SimpleTextInput = styled(Input)`
   background-color: transparent;
@@ -18,36 +23,61 @@ const SimpleTextInput = styled(Input)`
 const EchoInput = styled(Input)`
   position: absolute;
   color: red;
-  width: 100%;
-  height: 100%;
+  /* Hide scrollbar*/
+  ::-webkit-scrollbar-thumb, ::-webkit-scrollbar-track { background-color: transparent }
+  scrollbar-color: transparent transparent;
+
+  left: 9px;
+
   input + & {
     line-height: 36px; // from Input
+    white-space: nowrap;
+    overflow: auto hidden;
+    padding: 0;
+    height: calc(100% + 18px);
+    right: 9px;
   }
+
   textarea + & {
     line-height: normal;
     white-space: pre-wrap; // important for pixel-perfect overlay
-    overflow: auto; // important for pixel-perfect overlay
-    /* resize: vertical; */
-    /* overflow-wrap: break-word; */
-    padding: 5.5px 9px; // from Input
+    overflow: hidden auto; // important for pixel-perfect overlay
+    padding: 0 9px 5.5px 0; // from Input
+    top: 5.5px;
+    height: calc(100% - 5.5px);
   }
 `
 
-const HighlightContainer = styled(FlexContainer)`
-  position: relative;
-  overflow: hidden;
-  width: 100%;
-`
+type CommonInputElement = HTMLInputElement | HTMLTextAreaElement
 
 const TextInputHighlight = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   props: any,
-  ref: React.ForwardedRef<HTMLInputElement | HTMLTextAreaElement>
+  ref: React.ForwardedRef<CommonInputElement>
 ) => {
   const external = useRequiredContext(ExternalServiceManagerContext)
+  const echoRef = useRef<HTMLDivElement>(null)
+  const inputRef = useRef(ref && "current" in ref ? ref.current : null) // use forwarded ref or create intermediate
+  
+  useEffect(() => {
+    const { current: echo } = echoRef
+    const { current: input } = inputRef
+    if (echo && input) {
+      const setScroll = () => {
+        echo.scrollTop  = input.scrollTop // echo.scrollHeight * (input.scrollTop  / input.scrollHeight)
+        echo.scrollLeft = input.scrollLeft // echo.scrollWidth  * (input.scrollLeft / input.scrollWidth)
+      }
+  
+      input.addEventListener("scroll", setScroll)
+      return () => {
+        input.removeEventListener("scroll", setScroll)
+      }
+    }
+  }, [inputRef])
+  
   return <HighlightContainer>
-    <SimpleTextInput ref={ref} {...props} />
-    <EchoInput as="div">{props.value}</EchoInput>
+    <SimpleTextInput ref={ref ?? inputRef} {...props} />
+    <EchoInput ref={echoRef} as="div">{props.id !== "webhook" && props.value}</EchoInput>
   </HighlightContainer>
 }
 
