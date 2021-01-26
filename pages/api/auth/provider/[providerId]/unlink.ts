ce0e2f42138357e0c7eb0d43407567bb21921e74
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-01-27 00:19:45 +0300

    Repair unlink

--- pages/api/auth/provider/[providerId]/unlink.ts
+++ pages/api/auth/provider/[providerId]/unlink.ts
@@ -5,42 +5,39 @@ import { Session } from "../../../../../collaborative/models/Session"
 import { User } from "../../../../../collaborative/models/User"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  // const session = await getCustomSession({ req })
-  // const { providerId } = req.query
+  const session = await getCustomSession({ req })
+  const { providerId } = req.query
 
-  // try {
-  //   if (
-  //     req.method === "POST" &&
-  //     (providerId === "discord" || providerId === "google") &&
-  //     session &&
-  //     session[providerId]
-  //   ) {
-  //     const accs = await Account.find({
-  //       where: {
-  //         userId: session.id,
-  //       },
-  //     })
-  //     const accToDelete = accs.find(
-  //       a =>
-  //         a.providerId === providerId &&
-  //         a.providerAccountId ===
-  //           (providerId === "discord"
-  //             ? session.discord!.id
-  //             : session.google!.sub),
-  //     )
+  try {
+    if (
+      req.method === "POST" &&
+      providerId === "google" &&
+      session &&
+      session[providerId]
+    ) {
+      const accs = await Account.find({
+        where: {
+          userId: session.id,
+        },
+      })
+      const accToDelete = accs.find(
+        a =>
+          a.providerId === providerId &&
+          a.providerAccountId === session.google!.sub
+      )
 
-  //     await accToDelete?.remove()
+      await accToDelete?.remove()
 
-  //     if (accToDelete && accs.length === 1) {
-  //       await User.delete({ id: session.id })
-  //       await Session.delete({ sessionToken: session.accessToken })
-  //       return res.status(204).end()
-  //     }
+      if (accToDelete && accs.length === 1) {
+        await User.delete({ id: session.id })
+        await Session.delete({ sessionToken: session.accessToken })
+        return res.status(204).end()
+      }
 
-  //     return res.status(200).end()
-  //   }
-  //   throw ""
-  // } catch {
-  //   return res.status(404).end()
-  // }
+      return res.status(200).end()
+    }
+    throw ""
+  } catch {
+    return res.status(404).end()
+  }
 }
