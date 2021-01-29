9c1d548fbe388d5baa2f15cb002c065fee9e2324
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-01-29 12:52:00 +0300

    Refactor form to parts

--- collaborative/modals/parts/PostSelector.tsx
+++ collaborative/modals/parts/PostSelector.tsx
@@ -1,1 +1,29 @@
-export {}
+import { useObserver } from "mobx-react-lite"
+import React from "react"
+import { useQuery } from "react-query"
+import { PrimaryButton } from "../../../common/input/button/PrimaryButton"
+import { useRequiredContext } from "../../../common/state/useRequiredContext"
+import type { PostsAPIResponce } from "../../../pages/api/google/posts"
+import { fetchResource } from "../../helpers/query"
+import { Dropdown } from "../../layout/Dropdown"
+import { DropdownRow } from "../../layout/DropdownRow"
+import { CollaborationManagerContext } from "../../manager/CollaborationManagerContext"
+
+export const PostSelector = () => {
+    const collaborationManager = useRequiredContext(CollaborationManagerContext)
+
+    const spreadsheetId = collaborationManager.spreadsheet?.id ?? ""
+    const channelId = collaborationManager.channel?.id ?? ""
+    
+    const { data: res, isSuccess, isLoading } = useQuery(
+        [spreadsheetId, channelId, "posts"],
+        fetchResource<PostsAPIResponce>("/api/google/posts", { spreadsheetId, channelId })
+    )
+    return useObserver(() => <>
+        Select a post:
+        <DropdownRow>
+            <Dropdown loading={isLoading} options={[]}/>
+            <PrimaryButton>Create new</PrimaryButton>
+        </DropdownRow>
+    </>)
+}
