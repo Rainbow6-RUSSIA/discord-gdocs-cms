5e935c620eb9d389b3bd857eb0e250ffa972a156
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-02-07 03:27:13 +0300

    Deploy trimmed Convergence omnibus

---
+++ collaborative/convergence/omnibus/convergence-admin-console.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable prefer-template */
+/* eslint-disable unicorn/filename-case */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// const origin = window.location.origin;
+
+// const CONVERGENCE_ADMIN_CONSOLE_CONFIG = {
+//   baseUrl: "/console/",
+//   restApiUrl: `${origin  }/api/rest/`,
+//   realtimeApiUrl: `${origin  }/api/realtime/`
+// }
