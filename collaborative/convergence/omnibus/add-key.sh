3072ea594699f8c960b326e1218146e0270cba1f
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-02-09 05:24:05 +0300

    Add default public key via OrientDB REST API

---
+++ collaborative/convergence/omnibus/add-key.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+sleep 15s
+
+PUBLIC_KEY=$(cat public.key)
+echo "$PUBLIC_KEY"
+
+AUTH="root:password"
+
+DATABASE_NAME=$(curl "http://$AUTH@localhost:2480/query/convergence/sql/SELECT%20databaseName%20from%20Domain%20WHERE%20id=%22default%22" | jq ".result[0].databaseName" | cut -d '"' -f 2)
+echo "$DATABASE_NAME"
+
+curl -X POST "http://$AUTH@localhost:2480/command/$DATABASE_NAME/sql" \
+    -H "Content-Type: application/json" \
+    --data "{
+        \"command\":\"insert into JwtAuthKey (description, id, updated, key, enabled) VALUES (\"Default Key\", \"jwtkey\", date(), ?, true)\",
+        \"parameters\": [\"$PUBLIC_KEY\"]
+    }"
