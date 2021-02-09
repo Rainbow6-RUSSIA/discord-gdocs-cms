e88a6d109ffd4815279e36d8635bdffe39ffc3ae
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-02-09 11:14:51 +0300

    Add key autoretry

--- collaborative/convergence/omnibus/add-key.sh
+++ collaborative/convergence/omnibus/add-key.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env sh
 
-sleep 1m
+sleep 5s
 
 PUBLIC_KEY=$(cat public.key)
-echo "$PUBLIC_KEY"
+echo "WRITING PUBLIC KEY..."
 
 AUTH="root:password"
 
-DATABASE_NAME=$(curl "http://$AUTH@localhost:2480/query/convergence/sql/SELECT%20databaseName%20from%20Domain%20WHERE%20id=%22default%22" | jq ".result[0].databaseName" | cut -d '"' -f 2)
-echo "$DATABASE_NAME"
+DATABASE_NAME=$(curl -s "http://$AUTH@localhost:2480/query/convergence/sql/SELECT%20databaseName%20from%20Domain%20WHERE%20id=%22default%22" | jq ".result[0].databaseName" | cut -d '"' -f 2)
 
-curl -X POST "http://$AUTH@localhost:2480/command/$DATABASE_NAME/sql" \
+RESPONCE_CODE=$(curl -X POST "http://$AUTH@localhost:2480/command/$DATABASE_NAME/sql" \
+    -so /dev/null -w '%{response_code}' \
     -H "Content-Type: application/json" \
     --data "{
         \"command\":\"insert into JwtAuthKey (description, id, updated, key, enabled) VALUES (\"Default Key\", \"defaultkeyid\", date(), ?, true)\",
         \"parameters\": [\"$PUBLIC_KEY\"]
-    }"
+    }")
+echo "SERVER RESPONCE CODE: $RESPONCE_CODE"
+
+if [ $RESPONCE_CODE -eq 200 ] 
+then
+    echo "ADD KEY SUCCESS"
+    exit 0
+else
+    echo "ADD KEY FAILED"
+    exit 1
+fi
