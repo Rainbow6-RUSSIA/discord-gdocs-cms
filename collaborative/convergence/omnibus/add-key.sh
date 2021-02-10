78aad5a000c3e2b64e6d8290e602676b2461b9cb
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-02-10 22:44:37 +0300

    Add key impovement

--- collaborative/convergence/omnibus/add-key.sh
+++ collaborative/convergence/omnibus/add-key.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env sh
 
-sleep 1m
+sleep 10s
 
-PUBLIC_KEY=$(cat public.key)
 echo "WRITING PUBLIC KEY..."
 
+PUBLIC_KEY=$(cat public.key)
 AUTH="root:password"
+QUERY_RES=$(curl -s "http://$AUTH@localhost:2480/query/convergence/sql/SELECT%20status%2C%20databaseName%20from%20Domain%20WHERE%20id%3D%22default%22")
+STATUS=$(echo "$QUERY_RES" | jq ".result[0].status" | cut -d '"' -f 2)
+DATABASE_NAME=$(echo "$QUERY_RES" | jq ".result[0].databaseName" | cut -d '"' -f 2)
 
-DATABASE_NAME=$(curl -s "http://$AUTH@localhost:2480/query/convergence/sql/SELECT%20databaseName%20from%20Domain%20WHERE%20id=%22default%22" | jq ".result[0].databaseName" | cut -d '"' -f 2)
+if [ $STATUS != "online" ] 
+then
+    echo "SERVER STILL STARTING"
+    exit 1
+fi
 
 RESPONCE_CODE=$(curl -X POST "http://$AUTH@localhost:2480/command/$DATABASE_NAME/sql" \
     -so /dev/null -w '%{response_code}' \
