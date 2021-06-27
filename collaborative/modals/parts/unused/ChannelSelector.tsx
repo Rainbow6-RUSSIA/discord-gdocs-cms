0e78a5edc2b23fd4ba1b2a36146db0670181cc47
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-06-28 00:16:46 +0300

    Disable selectors

---
+++ collaborative/modals/parts/unused/ChannelSelector.tsx
@@ -0,0 +1,52 @@
+// import { toJS } from "mobx"
+// import { useObserver } from "mobx-react-lite"
+// import React from "react"
+// import { useQuery } from "react-query"
+// import { PrimaryButton } from "../../../../common/input/button/PrimaryButton"
+// import { useRequiredContext } from "../../../../common/state/useRequiredContext"
+// import type { ChannelsAPIResponce } from "../../../../pages/api/google/channels"
+// import { fetchResource, optionMap } from "../../../helpers/query"
+// import { Dropdown, DropdownOptions } from "../../../layout/Dropdown"
+// import { DropdownRow } from "../../../layout/DropdownRow"
+// import { CollaborationManagerContext } from "../../../manager/CollaborationManagerContext"
+// import { PostSelector } from "./PostSelector"
+
+// export const ChannelSelector = () => {
+//   const collaborationManager = useRequiredContext(CollaborationManagerContext)
+
+//   const spreadsheetId = collaborationManager.spreadsheet?.id ?? ""
+
+//   const { data: res, isSuccess, isLoading } = useQuery(
+//     [spreadsheetId, "channels"],
+//     fetchResource<ChannelsAPIResponce>("/api/google/channels", {
+//       spreadsheetId,
+//     }),
+//   )
+
+//   const optionsChannels: DropdownOptions = res
+//     ? res.data.map(ch => ({ ...ch, name: `#${ch.name}` })).map(optionMap)
+//     : []
+
+//   const selectChannel = (e: React.ChangeEvent<HTMLSelectElement>) => {
+//     collaborationManager.channel = res?.data.find(
+//       r => r.id === e.currentTarget.selectedOptions[0].value,
+//     )
+//     collaborationManager.saveSettings()
+//     console.log(toJS(collaborationManager.channel))
+//   }
+
+//   return useObserver(() => (
+//     <>
+//       Select Discord channel:
+//       <Dropdown
+//         onChange={selectChannel}
+//         placeholder="none"
+//         loading={isLoading}
+//         options={optionsChannels}
+//       />
+//       {isSuccess && collaborationManager.channel && <PostSelector />}
+//     </>
+//   ))
+// }
+
+export { }
---
+++ collaborative/modals/parts/unused/SpreadsheetSelector.tsx
@@ -0,0 +1,62 @@
+// import { toJS } from "mobx"
+// import { useObserver } from "mobx-react-lite"
+// import React from "react"
+// import { useQuery } from "react-query"
+// import { PrimaryButton } from "../../../../common/input/button/PrimaryButton"
+// import { useRequiredContext } from "../../../../common/state/useRequiredContext"
+// import type { SpreadsheetsAPIResponce } from "../../../../pages/api/google/spreadsheets"
+// import { fetchResource, optionMap } from "../../../helpers/query"
+// import { Dropdown, DropdownOptions } from "../../../layout/Dropdown"
+// import { DropdownRow } from "../../../layout/DropdownRow"
+// import { CollaborationManagerContext } from "../../../manager/CollaborationManagerContext"
+// import { ChannelSelector } from "./ChannelSelector"
+
+// // TODO: option processor
+// // mark saved selection stale if doesn't received
+
+// export const SpreadsheetSelector = () => {
+//   const collaborationManager = useRequiredContext(CollaborationManagerContext)
+
+//   const { data: res, isSuccess, isLoading } = useQuery(
+//     "spreadsheets",
+//     fetchResource<SpreadsheetsAPIResponce>("/api/google/spreadsheets"),
+//   )
+
+//   const selectSpreadsheet = (e: React.ChangeEvent<HTMLSelectElement>) => {
+//     collaborationManager.spreadsheet = res?.data.find(
+//       r => r.id === e.currentTarget.selectedOptions[0].value,
+//     )
+//     collaborationManager.saveSettings()
+//     console.log(toJS(collaborationManager.spreadsheet))
+//   }
+
+//   const optionsSpreadsheets: DropdownOptions = res
+//     ? [
+//       {
+//         group: res.data.filter(f => f.starred).map(optionMap),
+//         name: "Starred",
+//       },
+//       ...res.data.filter(f => !f.starred).map(optionMap),
+//     ]
+//     : []
+
+//   return useObserver(() => (
+//     <>
+//       Select a spreadsheet:
+//       <DropdownRow>
+//         <Dropdown
+//           onChange={selectSpreadsheet}
+//           placeholder="none"
+//           loading={isLoading}
+//           options={optionsSpreadsheets}
+//         />
+//         <PrimaryButton>Create new</PrimaryButton>
+//       </DropdownRow>
+//       {Boolean(isSuccess && collaborationManager.spreadsheet) && (
+//         <ChannelSelector />
+//       )}
+//     </>
+//   ))
+// }
+
+export { }
