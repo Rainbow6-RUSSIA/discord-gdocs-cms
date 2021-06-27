0450b4e443d6f4f4f44834d2e7158c07ccdf81de
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-06-28 00:16:01 +0300

    WIP unlink

--- pages/api/auth/provider/[providerId]/unlink.ts
+++ pages/api/auth/provider/[providerId]/unlink.ts
@@ -1,36 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-import { Account } from "../../../../../collaborative/auth/models/Account"
-import { Session } from "../../../../../collaborative/auth/models/Session"
-import { User } from "../../../../../collaborative/auth/models/User"
-import { getCustomSession } from "../../../../../collaborative/auth/session"
+import { getSession } from "next-auth/client"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getCustomSession({ req })
+  const session = await getSession({ req })
   const { providerId } = req.query
 
   try {
-    const account = session?.accounts.find(a => a.type === providerId)
-    if (req.method === "POST" && session && account) {
-      const accs = await Account.find({
-        where: {
-          userId: session.id,
-        },
-      })
-      const accToDelete = accs.find(
-        a => a.providerId === providerId && a.providerAccountId === account.id,
-      )
+    throw new Error("Not implemented")
+    // const account = session?.accounts.find(a => a.type === providerId)
+    // if (req.method === "POST" && session && account) {
+    //   const accs = await Account.find({
+    //     where: {
+    //       userId: session.id,
+    //     },
+    //   })
+    //   const accToDelete = accs.find(
+    //     a => a.providerId === providerId && a.providerAccountId === account.id,
+    //   )
 
-      await accToDelete?.remove()
+    //   await accToDelete?.remove()
 
-      if (accToDelete && accs.length === 1) {
-        await User.delete({ id: session.id })
-        await Session.delete({ sessionToken: session.accessToken })
-        return res.status(204).end()
-      }
+    //   if (accToDelete && accs.length === 1) {
+    //     await User.delete({ id: session.id })
+    //     await Session.delete({ sessionToken: session.accessToken })
+    //     return res.status(204).end()
+    //   }
 
-      return res.status(200).end()
-    }
-    throw ""
+    //   return res.status(200).end()
+    // }
+    // throw ""
   } catch (error) {
     console.log(error)
     return res.status(500).end()
