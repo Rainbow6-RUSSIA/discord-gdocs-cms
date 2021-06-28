09c8b69640fa44ce7de90ae65153e2d0de29acea
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-06-28 23:03:41 +0300

    Supress full logout error

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,5 +6,14 @@ import { adapter } from "../../../collaborative/db"
 export default NextAuth({
   providers,
   adapter,
-  callbacks
+  callbacks,
+  logger: {
+    error: (errorCode, ...text) => {
+      if (["SIGNOUT_ERROR", "PRISMA__DELETE_SESSION_ERROR"].includes(errorCode)) return
+      console.log(errorCode, ...text)
+    },
+    // eslint-disable-next-line no-underscore-dangle
+    debug: process.env._NEXTAUTH_DEBUG ? console.debug : () => { },
+    warn: console.warn
+  }
 })
