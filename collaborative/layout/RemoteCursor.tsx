d69288caae73bb25a47037aa53d71b343d887c18
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-07-02 16:02:18 +0300

    Cursor blink

--- collaborative/layout/RemoteCursor.tsx
+++ collaborative/layout/RemoteCursor.tsx
@@ -1,39 +1,23 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 
 export type RemoteCursorProps = {
-    color: string
-    // name: string
+  color: string
+  // name: string
 }
 
 export const RemoteSelection = styled.span<RemoteCursorProps>`
-    position: relative;
-    background-color: ${props => `${props.color}7F`};
-    box-shadow: 0 0 0 1px ${props => props.color};
-    border-radius: 2px;
-  `
+  position: relative;
+  background-color: ${props => `${props.color}7F`};
+  box-shadow: 0 0 0 1px ${props => props.color};
+  border-radius: 2px;
+`
+
+const blink = keyframes`
+  from, to { box-shadow: none; }
+  50% { box-shadow: 0 0 0 1px currentColor; }
+`
 
 export const RemoteCursor = styled(RemoteSelection)`
-    /* &:before { // this solution disappointed me because cursor name under the borders of input 
-      position: absolute;
-      font-size: 14px;
-      line-height: 14px;
-      top: -14px;
-      left: -2px;
-      content: "${props => props.color}";
-      background: ${props => props.color};
-      height: 14px;
-      z-index: 2;
-    } */
-    &:before {
-      position: absolute;
-      top: 0;
-      z-index: 2;
-      content: "";
-  
-      width: 0; // CSS Triangle
-      height: 0;
-      border-style: solid;
-      border-width: 2px 0 2px 8px;
-      border-color: transparent transparent transparent ${props => props.color};
-    }
-  `
+  animation: ${blink} 1s step-end infinite;
+  color: ${props => props.color};
+`
